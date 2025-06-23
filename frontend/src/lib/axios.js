40f55ae44ad52f139dd8fe0ff002e44cc0405d51
ApexArbiter <mahadwebdev@gmail.com>
2025-06-23 09:19:29 +0500

    Hello world

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-export const axoisInstance = axios.create({
+const axiosInstance = axios.create({
   baseURL: "http://localhost:5001/api",
   withCredentials: true,
 });
+
+export default axiosInstance; 
---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,36 @@
+import { create } from "zustand";
+
+import toast from "react-hot-toast";
+
+import axiosInstance from "../lib/axios.js";
+
+export const useChatStore = create((set) => ({
+  messages: [],
+  users: [],
+  selectedUser: null,
+  isUserLoading: false,
+  isMessagesLoading: false,
+
+  getUsers: async () => {
+    set({ isUserLoading: true });
+    try {
+      const res = await axiosInstance.get("/message/users");
+      set({ users: res.data });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUserLoading: false });
+    }
+  },
+  getMessages: async (userid) => {
+    set({ isMessagesLoading: true });
+    try {
+      const res = await axiosInstance.get(`/messages/${userid}`);
+      set({ messages: res.data });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isMessagesLoading: false });
+    }
+  },
+}));
